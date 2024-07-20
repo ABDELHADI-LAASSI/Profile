8f1bc2f893153a6d567f1fc771d0139a8c749a3c
abdelhadi <twitchabdelhadi@gmail.com>
2024-07-20 13:36:08 +0100

    x

---
+++ src/home/social.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://www.instagram.com/abdelhadilaassi/" className="home__social-icon" target='_blank'>
+                <i className='uil uil-instagram'></i>
+            </a>
+
+
+            <a href="https://twitter.com/abdelhadiLaassi" className="home__social-icon" target='_blank'>
+                <i className='bx bxl-twitter'></i>
+            </a>
+
+
+            <a href="https://github.com/ABDELHADI-LAASSI" className="home__social-icon" target='_blank'>
+                <i className='uil uil-github-alt'></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social
---
+++ src/projects/Data.jsx
@@ -0,0 +1,112 @@
+import Work1 from "../assets/web1.png"
+import Work2 from "../assets/web2.png"
+import Work3 from "../assets/web3.png"
+import Work4 from "../assets/web4.png"
+import Work5 from "../assets/web5.png"
+import Work6 from "../assets/web6.png"
+import Work7 from "../assets/web7.png"
+
+export const projectsData = [
+    {
+        id: 1,
+        image: Work4,
+        title: "Web design",
+        category: "html_css",
+        link :"https://65b4e29a51be0c8263cbb7f5--wondrous-begonia-fc88f5.netlify.app/",
+    },
+    {
+        id: 2,
+        image: Work1,
+        title: "responsive business intelligence",
+        category: "html_css",
+        link :"https://65b4dcc1c23cc38b9929b9d1--imaginative-sunburst-3a40d9.netlify.app/",
+        
+    },
+    {
+        id: 3,
+        image: Work2,
+        title: "responsive technology cards",
+        category: "html_css",
+        link :"https://65b4dd3b2ddcf9811fabc3c6--capable-biscuit-727307.netlify.app/",
+    },
+    {
+        id: 4,
+        image: Work3,
+        title: "responsive customers openions",
+        category: "html_css",
+        link :"https://65b4e02cc23cc38e4a29b9c6--harmonious-bonbon-ae075e.netlify.app/",
+    },
+    
+    {
+        id: 5,
+        image: Work5,
+        title: "calculator",
+        category: "javaScript",
+        link :"https://65b4e5024d919a852bbf5357--heroic-capybara-a1ac41.netlify.app/",
+    },
+    {
+        id: 6,
+        image: Work6,
+        title: "todo List",
+        category: "react",
+        link :"https://65b4ee9da5aef388e36e390d--eloquent-babka-3726de.netlify.app/",
+    },
+    
+    {
+        id: 7,
+        image: Work7,
+        title: "managing users and their publications",
+        category: "laravel",
+        link :"",
+    },
+    
+];
+// export const projectsData = [
+//         {
+//         id: 1,
+//         image: Work1,
+//         title: "Web design",
+//         category: "web",
+//         },
+//         {
+//         id: 2,
+//         image: Work2,
+//         title: "App movil",
+//         category: "app",
+//         },
+//         {
+//         id: 3,
+//         image: Work3,
+//         title: "Brand design",
+//         category: "design",
+//         },
+//         {
+//         id: 4,
+//         image: Work4,
+//         title: "App movil",
+//         category: "app",
+//         },
+//         {
+//         id: 5,
+//         image: Work5,
+//         title: "Web design",
+//         category: "web",
+//         },
+
+export const projectNav = [
+    {
+        name:'all'
+    },
+    {
+        name:'html_css'
+    },
+    {
+        name:'javaScript'
+    },
+    {
+        name:'react'
+    },
+    {
+        name:'laravel'
+    },
+]
