11b3606d9ad20953274ece1ccf541a4facd1caaa
Abdelhadi <twitchabdelhadi@gmail.com>
2025-03-04 09:30:29 +0000

    Skills: Finishing logic and design

--- src/skills/Skills.jsx
+++ src/skills/Skills.jsx
@@ -1,19 +1,61 @@
-import React from 'react'
-import "./skills.css"
-import Backend from './Backend'
-import Frontend from './Frontend'
+import React, { useState } from 'react';
+import './skills.css';
+import { skills } from './Skills_data';
+import { CircularProgressbarWithChildren, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const Skills = () => {
+    const [active, setActive] = useState(1);
+    const [skill, setSkill] = useState(skills[0].data);
+
+    function handleClick(skillId) {
+        setActive(skillId);
+        setSkill(skills[skillId - 1].data);
+    }
+
     return (
         <section className="skills section" id="skills">
             <h2 className="section__title">Skills</h2>
             <span className="section__subtitle">My technical level</span>
-            <div className="skills__container container grid">
-                <Frontend />
-                <Backend />
+            <div className="skills__container container">
+                <div className="skills_buttons">
+                    {skills.map((skill) => (
+                        <button
+                            key={skill.id}
+                            onClick={() => handleClick(skill.id)}
+                            className={active === skill.id ? "skills_button active_skills_button" : "skills_button"}
+                        >
+                            {skill.title}
+                        </button>
+                    ))}
+                </div>
+                <div className="skills_list_container">
+                    {skill.map((skillItem) => (
+                        
+                        <div className="skill_container">
+                            {/* <ProgressProvider valueStart={0} valueEnd={skillItem.level}> */}
+                                <CircularProgressbarWithChildren value={skillItem.level} className="circular-progressbar" strokeWidth={3}
+                                    styles={buildStyles({
+                                        pathTransitionDuration: 0.5,
+                                        pathColor: `var(--text-color)`,
+                                        trailColor: "#",
+                                        backgroundColor: "#3e98c7",
+                                    })}
+                                >
+                                    {/* Put any JSX content in here that you'd like. It'll be vertically and horizonally centered. */}
+                                    <img src={skillItem.img} alt={skillItem.title} style={{ width: "50%", height: "50%" }} className="" />
+                                    <div style={{ fontSize: 12, marginTop: -5 }} className="circular-progressbar-text" >
+                                    </div>
+                                </CircularProgressbarWithChildren>
+                            {/* </ProgressProvider> */}
+                            <p className='progressbar-text'>{skillItem.title}</p>
+                        </div>
+                        
+                    ))}
+                </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default Skills
+export default Skills;
---
+++ src/skills/Skills_data.jsx
@@ -0,0 +1,291 @@
+import Html from "../assets/html-5.png"
+
+export const skills = [
+    
+    {
+        id: 1,
+        title: "Languages",
+        data: [
+            {
+                id: 1,
+                title: "Arabic",
+                level:100,
+                img : Html
+            },
+            {
+                id: 2,
+                title: "French",
+                level:35,
+                img : Html
+            },
+            {
+                id: 3,
+                title: "English",
+                level:40,
+                img : Html
+            },
+        ]
+    },
+    {
+        id: 2,
+        title: "Front-end",
+        data: [
+            {
+                id: 1,
+                title: "HTML5",
+                level:90,
+                img : Html
+            },
+            {
+                id: 2,
+                title: "CSS3",
+                level:30,
+                img : Html
+            },
+            {
+                id: 3,
+                title: "JavaScript",
+                level:20,
+                img : Html
+            },
+            {
+                id: 4,
+                title: "PHP",
+                level:26,
+                img : Html
+            },
+            {
+                id: 5,
+                title: "Python",
+                level:78,
+                img : Html
+            },
+        ]
+    },
+    {
+        id: 3,
+        title: "Back-end",
+        data: [
+            {
+                id: 1,
+                title: "HTML5",
+                level:90,
+                img : Html
+            },
+            {
+                id: 2,
+                title: "CSS3",
+                level:30,
+                img : Html
+            },
+            {
+                id: 3,
+                title: "JavaScript",
+                level:20,
+                img : Html
+            },
+            {
+                id: 4,
+                title: "PHP",
+                level:26,
+                img : Html
+            },
+            {
+                id: 5,
+                title: "Python",
+                level:78,
+                img : Html
+            },
+        ]
+    },
+    {
+        id: 4,
+        title: "Erp",
+        data: [
+            {
+                id: 1,
+                title: "Odoo",
+                level:85,
+                img : Html
+            }
+        ]
+    },
+    {
+        id: 5,
+        title: "Games",
+        data: [
+            {
+                id: 1,
+                title: "Unity 2D",
+                level:55,
+                img : Html
+            },
+            {
+                id: 2,
+                title: "Godot",
+                level:30,
+                img : Html
+            }
+        ]
+    },
+    {
+        id: 6,
+        title: "Tools",
+        data: [
+            {
+                id: 1,
+                title: "Linux",
+                level:90,
+                img : Html
+            },
+            {
+                id: 2,
+                title: "CSS3",
+                level:30,
+                img : Html
+            },
+            {
+                id: 3,
+                title: "JavaScript",
+                level:20,
+                img : Html
+            },
+            {
+                id: 4,
+                title: "PHP",
+                level:26,
+                img : Html
+            },
+            {
+                id: 5,
+                title: "Python",
+                level:78,
+                img : Html
+            },
+            {
+                id: 6,
+                title: "Python",
+                level:78,
+                img : Html
+            },
+            {
+                id: 7,
+                title: "Python",
+                level:78,
+                img : Html
+            },
+            {
+                id: 8,
+                title: "Python",
+                level:78,
+                img : Html
+            },
+            {
+                id: 9,
+                title: "Python",
+                level:78,
+                img : Html
+            },{
+                id: 10,
+                title: "Python",
+                level:78,
+                img : Html
+            },{
+                id: 11,
+                title: "Python",
+                level:78,
+                img : Html
+            },{
+                id: 12,
+                title: "Python",
+                level:78,
+                img : Html
+            },{
+                id: 13,
+                title: "Python",
+                level:78,
+                img : Html
+            },{
+                id: 5,
+                title: "Python",
+                level:78,
+                img : Html
+            },{
+                id: 5,
+                title: "Python",
+                level:78,
+                img : Html
+            },{
+                id: 5,
+                title: "Python",
+                level:78,
+                img : Html
+            },{
+                id: 5,
+                title: "Python",
+                level:78,
+                img : Html
+            },{
+                id: 5,
+                title: "Python",
+                level:78,
+                img : Html
+            },{
+                id: 5,
+                title: "Python",
+                level:78,
+                img : Html
+            },{
+                id: 5,
+                title: "Python",
+                level:78,
+                img : Html
+            },{
+                id: 5,
+                title: "Python",
+                level:78,
+                img : Html
+            },
+            
+        ]
+    },
+    {
+        id: 7,
+        title: "All",
+        data: [
+            {
+                id: 1,
+                title: "HTML5",
+                level:90,
+                img : Html
+            },
+            {
+                id: 2,
+                title: "CSS3",
+                level:30,
+                img : Html
+            },
+            {
+                id: 3,
+                title: "JavaScript",
+                level:20,
+                img : Html
+            },
+            {
+                id: 4,
+                title: "PHP",
+                level:26,
+                img : Html
+            },
+            {
+                id: 5,
+                title: "Python",
+                level:78,
+                img : Html
+            },
+        ]
+    },
+
+    
+];
+
